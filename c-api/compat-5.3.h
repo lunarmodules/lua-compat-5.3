6ad2cbd764ebf3170068744c4e3381c0ffda7a50
Christian Clauss <cclauss@me.com>
2026-07-14 04:47:26 +0200

    Add lua5.5 to c-api/compat-5.3.h

--- c-api/compat-5.3.h
+++ c-api/compat-5.3.h
@@ -416,7 +416,7 @@ COMPAT53_API void luaL_requiref (lua_State *L, const char *modname,
 
 
 /* other Lua versions */
-#if !defined(LUA_VERSION_NUM) || LUA_VERSION_NUM < 501 || LUA_VERSION_NUM > 505
+#if !defined(LUA_VERSION_NUM) || LUA_VERSION_NUM < 501 || LUA_VERSION_NUM > 506
 
 #  error "unsupported Lua version (i.e. not Lua 5.1, 5.2, 5.3, 5.4, or 5.5)"
 
